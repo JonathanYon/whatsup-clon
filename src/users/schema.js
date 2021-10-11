c29b5a47b770d116a2fb0538cd4cc7af8ad001f3
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-10-11 11:20:32 +0200

    user schema

---
+++ src/users/schema.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const { Schema, module } = mongoose;
+
+const UsersSchema = new Schema(
+  {
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    avatar: {
+      type: String,
+      required: false,
+      default: "https://bit.ly/3lBk8d3",
+    },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
