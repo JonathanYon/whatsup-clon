c09464c74e41b51f8bcae139124b0f56768f38f0
sars723 <sarasalomonn@gmail.com>
2021-10-11 20:21:44 +0200

    user schema

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import listEndpoints from "express-list-endpoints";
 import mongoose from "mongoose";
 import chatRouter from "./services/chats/index.js";
+import userRouter from "./services/users/index.js";
 
 const server = express();
 const port = process.env.PORT || 3001;
@@ -12,6 +13,7 @@ console.log("process---", process.env.MONGOS_CON);
 server.use(cors());
 server.use(express.json());
 
+server.use("users",userRouter)
 server.use("/chats",chatRouter)
 
 console.log(listEndpoints(server));
