2417abef0bb030426347f3663c38271f9a4b6e98
sars723 <sarasalomonn@gmail.com>
2021-10-11 15:25:45 +0200

    Update chat-schema.js

--- src/services/chats/chat-schema.js
+++ src/services/chats/chat-schema.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose'
 const {Schema, model} = mongoose
 
 const ChatSchema = new Schema({
-    members: { type: Schema.Types.ObjectId, ref: "User" },
-    history: { type: Schema.Types.ObjectId, ref: "Message" }
+    members: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    history:[ { type: Schema.Types.ObjectId, ref: "Message" }]
 })
 
 export default model("Chat", ChatSchema)
